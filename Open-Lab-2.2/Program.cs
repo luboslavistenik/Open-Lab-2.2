4150128e2d90a25eb85ad353e5c00ea04769453a
z18ista08 <z18ista08@Z00P1-18.SPSKNM>
2019-10-14 08:48:03 +0200

    Add project files.

---
+++ Open-Lab-2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Open_Lab_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] array1 = { 2.11F, 2.22F, 2.33F, 2.44F, 2.55F };
+            float count = 0;
+            foreach (float element in array1)
+                {
+                count++;
+                Console.WriteLine($"Element #{count}: {element}");
+                }
+            Console.WriteLine($"Number of elements: {count}");
+        }
+    }
+}
